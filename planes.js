5d417a827d5e68d3b6efd2967cf8e34f7c0a36f9
akaymelik <akaymelik@gmail.com>
2026-04-28 17:26:13 +0300

    Create planes.js

---
+++ planes.js
@@ -0,0 +1,30 @@
+const aircraftData = {
+    "ATR-42": {
+        capacity: 50,
+        fuel_consumption: 0.25,
+        range: 1500,
+        cruise_speed: 400,
+        price: 1200000 
+    },
+    "Boeing 737": {
+        capacity: 180,
+        fuel_consumption: 0.85,
+        range: 5000,
+        cruise_speed: 900,
+        price: 5000000
+    },
+    "Airbus A380": {
+        capacity: 500,
+        fuel_consumption: 12.0,
+        range: 15000,
+        cruise_speed: 900,
+        price: 250000000
+    },
+    "MC-21-400": {
+        capacity: 230,
+        fuel_consumption: 10.5,
+        range: 5500,
+        cruise_speed: 870,
+        price: 33750000
+    }
+};
